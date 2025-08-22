7ece03bc27db0ce59c5d22183f9f970396b6e480
imran <imranphynix56@gmail.com>
2025-08-22 12:02:19 +0530

    optimised images

--- client/src/pages/shopping/ProductDescription.jsx
+++ client/src/pages/shopping/ProductDescription.jsx
@@ -253,7 +253,7 @@ const ProductDescription = () => {
                 />
               )}
               <span>
-                {status === "loading"
+                {status === "laoding"
                   ? "Adding to cart..."
                   : "Add To Cart"}
               </span>
